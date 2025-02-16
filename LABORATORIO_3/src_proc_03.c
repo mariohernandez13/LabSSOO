363c86d867bdbbafa3225d16cb2dba300c931eab
Mario Hernandez <9300322@alumnos.ufv.es>
2025-02-16 17:05:23 +0100

    fix indentacion lab 3

--- LABORATORIO_3/src_proc_03.c
+++ LABORATORIO_3/src_proc_03.c
@@ -2,25 +2,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
-void crearProcesoHijo(int nivel) {
-if (nivel > 0) {
-pid_t pid = fork();
-if (pid == -1) {
-perror("Fork falló");
-exit(1);
-} else if (pid == 0) { // Proceso hijo
-printf("Nivel %d, PID: %d, PPID: %d\n", 5 - nivel, getpid(), getppid());
-crearProcesoHijo(nivel - 1);
-sleep(10); // Todos los procesos duermen después de que se haya creado toda la jerarquía
-exit(0);
-} else {
-wait(NULL);
+void crearProcesoHijo(int nivel)
+{
+    if (nivel > 0)
+    {
+        pid_t pid = fork();
+        if (pid == -1)
+        {
+            perror("Fork falló");
+            exit(1);
+        }
+        else if (pid == 0)
+        { // Proceso hijo
+            printf("Nivel %d, PID: %d, PPID: %d\n", 5 - nivel, getpid(), getppid());
+            crearProcesoHijo(nivel - 1);
+            sleep(10); // Todos los procesos duermen después de que se haya creado toda la jerarquía
+            exit(0);
+        }
+        else
+        {
+            wait(NULL);
+        }
+    }
 }
-}
-}
-int main() {
-printf("Proceso raíz, PID: %d\n", getpid());
-crearProcesoHijo(5);
-sleep(10); // El proceso raíz también duerme
-return 0;
+
+int main()
+{
+    printf("Proceso raíz, PID: %d\n", getpid());
+    crearProcesoHijo(5);
+    sleep(10); // El proceso raíz también duerme
+    return 0;
 }
