522be19764874fe676aa6a1b86baadee952e5ce6
ArgentJC <javier171003@gmail.com>
2025-03-07 11:46:04 +0100

    Update banco.c

--- Practica_1/banco.c
+++ Practica_1/banco.c
@@ -6,6 +6,7 @@
 #include <string.h>
 
 #define MAX_LINE_LENGTH 255
+#define MAX_LENGTH_NAME 50
 
 /// @brief Función que se llama para leer el archivo de configuración
 /// @return valor numérico que indica la validez de la lectura
@@ -41,7 +42,84 @@ int readFile() {
     }
 
     fclose(file);
-    return state;
+
+    return (state);
+}
+
+/// @brief Comprueba en el archivo de cuentas.dat que el id introducido no existe
+/// @param id Id introducido por el usuario en el resgistro
+/// @param flag Valor que indica si se está llegando a la función desde LogIn o desde registro: 0 = registro || 1 = LogIn
+/// @return Valor numérico que indica validez del Id: 0 = error en id // 1 = id valido
+int comprobarId(int id, int flag){
+
+    int validez = 1;
+    if (id < 100)
+        validez = 0;
+
+}
+
+/// @brief Menú de registro del Banco
+void registro(){
+
+    int id;
+    float saldo;
+    char nombre[MAX_LENGTH_NAME];
+    int comprobacion = 1;
+
+    do{
+        printf("Bienvenido al registro de SafeBank\n");
+        printf("Introduce tu nombre: (no se admiten más de 50 caracteres)\n");
+        fgets(nombre, sizeof(nombre), stdin);
+        printf("Introduce tu id: (a partir de 100)\n");
+        scanf("%d", id);
+        printf("Introduce tu saldo: \n");
+        scanf("%.2f", saldo);
+    }while(comprobarId(id, 0) || (nombre == NULL) || (strlen(nombre) > MAX_LENGTH_NAME));
+    
+}
+
+/// @brief Menú de logIn del Banco
+void logIn(){
+
+    int id;
+    int flg_log = 1;
+
+    do{
+        printf("Bienvenido al LogIn de SafeBank\n");
+        printf("Introduce tu id: (a partir de 100)\n");
+        scanf("%d", id);
+    }while(comprobarId(id,flg_log));
+    
+}
+
+/// @brief Menú de inicio del Banco SafeBank
+void menuBanco(){
+
+    int opcion = 0;
+
+    do{
+        printf("Bienvenido a SafeBank.\n");
+        printf("¿Qué desea hacer?\n");
+        printf("1. Inicio Sesión\n");
+        printf("2. Registro\n");
+        printf("3. Salir\n");
+        printf("Introduce una opción: ");
+        scanf("%d", opcion);
+
+        switch(opcion)
+        {
+            case 1: 
+                logIn();
+                break;
+            case 2: 
+                registro(); // Llamamos a funcion registro
+                break;
+            default:
+                break; 
+        }
+
+    }while(opcion != 3);
+
 }
 
 int main() {
@@ -91,6 +169,5 @@ int main() {
         close(fd[0]);  // Cierra el extremo de lectura
     }
 
-    return 0;
+    return (0);
 }
-
