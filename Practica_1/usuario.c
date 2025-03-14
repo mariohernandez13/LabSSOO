682bee6f125d194b747c9fe6f1c729b28a87b687
Mario Hernandez <9300322@alumnos.ufv.es>
2025-03-14 11:51:23 +0100

    update funciones usuarios

--- Practica_1/usuario.c
+++ Practica_1/usuario.c
@@ -1,43 +1,56 @@
 #include "banco.h"
 
-void operacionDevolucion(){
-
+void operacionDevolucion()
+{
 }
 
-void ejecutarOperacion(int opcion){
- 
+void operacionTransferencia()
+{
+}
+
+void operacionRetiro()
+{
+}
+
+void operacionConsultarSaldo()
+{
+}
+
+void ejecutarOperacion(int opcion)
+{
+
     pthread_t hilo;
 
     switch (opcion)
     {
-        case 1:
-            pthread_create(&hilo, NULL, operacionDevolucion, NULL);
-            pthread_join(hilo, NULL);
-            break;
-        case 2:
-            pthread_create(&hilo, NULL, operacionDevolucion, NULL);
-            pthread_join(hilo, NULL);
-            break;
-        case 3:
-            pthread_create(&hilo, NULL, operacionDevolucion, NULL);
-            pthread_join(hilo, NULL);
-            break;
-        case 4:
-            pthread_create(&hilo, NULL, operacionDevolucion, NULL);
-            pthread_join(hilo, NULL);
-            break;
-        default:
-            break;
+    case 1:
+        pthread_create(&hilo, NULL, operacionDevolucion, NULL);
+        pthread_join(hilo, NULL);
+        break;
+    case 2:
+        pthread_create(&hilo, NULL, operacionTransferencia, NULL);
+        pthread_join(hilo, NULL);
+        break;
+    case 3:
+        pthread_create(&hilo, NULL, operacionRetiro, NULL);
+        pthread_join(hilo, NULL);
+        break;
+    case 4:
+        pthread_create(&hilo, NULL, operacionConsultarSaldo, NULL);
+        pthread_join(hilo, NULL);
+        break;
+    default:
+        break;
     }
-
 }
 
 /// @brief Función que realiza el menu de Usuario
-void menuUsuario(char *id){
+void menuUsuario(char *id)
+{
 
     int opcion = 0;
 
-    while (1)
+    do
     {
         printf("Bienvenido %s!\n", id);
         printf("1. Devolución\n");
@@ -46,21 +59,10 @@ void menuUsuario(char *id){
         printf("4. Consultar saldo\n");
         printf("5. Salir\n");
         printf("Inserte la acción que quiere ejecutar:\n");
-        
+
         scanf("%d", &opcion);
-        
-        switch (opcion){
-            case 1:
-            case 2: 
-            case 3: 
-            case 4:
-                ejecutarOperacion(opcion);
-                break;
-            case 5:
-                exit(0);
-                break;
-        }
-    }
+        ejecutarOperacion(opcion);
+    } while (opcion != 5);
 }
 
 int main(char *id, CONFIG configuracion)
